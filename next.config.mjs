bd37c642ef0552f2fa0505ccaba2fb983534b523
JP <creativesolutionsaillc@gmail.com>
2024-12-19 16:02:02 -0500

    Disable ESLint checks during the production build in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,21 @@ import mdx from '@next/mdx';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
+  extension: /\.mdx?$/,
+  options: {},
 });
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+
+  // Add ESLint configuration here
+  eslint: {
+    // Disables ESLint checks during the production build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextIntl(withMDX(nextConfig));
